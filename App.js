acd85d17bd5eb4b3afc3b3fc60a925685fd24f88
Jack <36623994+jackWhtman@users.noreply.github.com>
2025-01-10 15:53:50 +0530

    init

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from './src/screens/HomeScreen';
+import BreathingScreen from './src/screens/BreathingScreen';
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Breathing" component={BreathingScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
